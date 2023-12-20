1cb14fe0387153cbcf4a39eb35b9ed751deb13b7
dispel-re <141545384+dispel-re@users.noreply.github.com>
2023-12-20 18:54:40 +0100

    ♻️ Update go-module. Name missing character info.

--- console/console.go
+++ console/console.go
@@ -3,48 +3,109 @@ package console
 import (
 	"context"
 	"errors"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
 	"time"
 
 	"connectrpc.com/connect"
+	"connectrpc.com/otelconnect"
 	"github.com/dispel-re/dispel-multi/backend"
 	"github.com/dispel-re/dispel-multi/console/database"
+	"github.com/dispel-re/dispel-multi/gen/multi/v1/multiv1connect"
+	"github.com/dispel-re/dispel-multi/model"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/riandyrn/otelchi"
+	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+)
 
-	"connectrpc.com/otelconnect"
-	"github.com/dispel-re/dispel-multi/gen/multi/v1/multiv1connect"
+var (
+	healthy int32
 )
 
 type Console struct {
 	DB      *database.Queries
 	Backend *backend.Backend
+
+	CORSAllowedOrigins []string
 }
 
 func NewConsole(db *database.Queries, b *backend.Backend) *Console {
 	return &Console{
-		DB:      db,
-		Backend: b,
+		DB:                 db,
+		Backend:            b,
+		CORSAllowedOrigins: []string{"*"}, // TODO: For production replace it with []string{"https://dispel-multi.net"}
 	}
 }
 
 func (c *Console) Serve(ctx context.Context, consoleAddr, backendAddr string) error {
-	interceptors := connect.WithInterceptors(otelconnect.NewInterceptor())
+	mux := chi.NewRouter()
 
-	api := http.NewServeMux()
-	api.Handle(multiv1connect.NewCharacterServiceHandler(&characterServiceServer{DB: c.DB}, interceptors))
-	api.Handle(multiv1connect.NewGameServiceHandler(&gameServiceServer{DB: c.DB}, interceptors))
-	api.Handle(multiv1connect.NewUserServiceHandler(&userServiceServer{DB: c.DB}, interceptors))
-	api.Handle(multiv1connect.NewRankingServiceHandler(&rankingServiceServer{DB: c.DB}, interceptors))
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.DefaultLogger)
+	mux.Use(middleware.Throttle(100))
+	mux.Use(middleware.Timeout(5 * time.Second))
+	mux.Use(otelchi.Middleware("console", otelchi.WithChiRoutes(mux)))
 
-	mux := http.NewServeMux()
-	mux.Handle("/_health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) }))
-	mux.Handle("/_metrics", promhttp.Handler())
-	mux.Handle("/grpc/", http.StripPrefix("/grpc", api))
+	{ // Setup meta routes (readiness, liveness, metrics etc.)
+		mux.Get("/_health", func(w http.ResponseWriter, r *http.Request) {
+			if atomic.LoadInt32(&healthy) == 0 {
+				renderJSON(w, r, map[string]string{"status": "OK"})
+				return
+			}
+			w.WriteHeader(http.StatusServiceUnavailable)
+		})
+		mux.Get("/_metrics", promhttp.Handler().ServeHTTP)
+	}
+
+	{ // Setup routes used by the launcher
+		mux.Get("/.well-known/dispel-multi.json", func(w http.ResponseWriter, r *http.Request) {
+			renderJSON(w, r, model.WellKnown{
+				ZeroTier: model.ZeroTier{Enabled: false},
+			})
+		})
+	}
+
+	{ // Setup gRPC routes for the backend
+		api := chi.NewRouter()
+		api.Use(cors.New(cors.Options{
+			AllowedOrigins:   c.CORSAllowedOrigins,
+			AllowCredentials: false,
+			Debug:            false,
+			AllowedMethods: []string{
+				http.MethodGet,
+				http.MethodPost,
+			},
+			AllowedHeaders: []string{
+				"Content-Type",
+				"Connect-Protocol-Version",
+				"Connect-Timeout-Ms",
+				"Grpc-Timeout",
+				"X-Grpc-Web",
+				"X-User-Agent",
+			},
+			ExposedHeaders: []string{
+				"Grpc-Status",
+				"Grpc-Message",
+				"Grpc-Status-Details-Bin",
+			},
+			MaxAge: 7200,
+		}).Handler)
+
+		interceptors := connect.WithInterceptors(otelconnect.NewInterceptor())
+		api.Mount(multiv1connect.NewCharacterServiceHandler(&characterServiceServer{DB: c.DB}, interceptors))
+		api.Mount(multiv1connect.NewGameServiceHandler(&gameServiceServer{DB: c.DB}, interceptors))
+		api.Mount(multiv1connect.NewUserServiceHandler(&userServiceServer{DB: c.DB}, interceptors))
+		api.Mount(multiv1connect.NewRankingServiceHandler(&rankingServiceServer{DB: c.DB}, interceptors))
+		mux.Mount("/grpc/", http.StripPrefix("/grpc", api))
+	}
 
 	server := &http.Server{
 		Addr:    consoleAddr,
@@ -59,6 +120,9 @@ func (c *Console) Serve(ctx context.Context, consoleAddr, backendAddr string) er
 		}
 
 		// TODO: Set readiness, startup, liveness probe
+		atomic.StoreInt32(&healthy, 0)
+		slog.Info("Starting console server", "addr", consoleAddr)
+
 		return server.ListenAndServe()
 	}
 
